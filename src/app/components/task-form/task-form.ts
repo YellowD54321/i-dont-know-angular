4994dc2d0c62575be91f51f8462e0633b092cbe6
Allen Huang <allen.h@foospace.co>
2026-01-28 23:42:20 +0800

    implement task-form component with FormGroup and FormControl structure

--- src/app/components/task-form/task-form.ts
+++ src/app/components/task-form/task-form.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './task-form.html',
   styleUrl: './task-form.css',
 })
-export class TaskForm {}
+export class TaskForm {
+  private fb = inject(FormBuilder);
+
+  taskForm = this.fb.group({
+    title: ['', [Validators.required, Validators.minLength(5)]],
+    description: [''],
+    priority: ['medium' as 'low' | 'medium' | 'high'],
+    subTasks: this.fb.array([]),
+    tags: this.fb.array([]),
+  });
+
+  get subTasks(): FormArray {
+    return this.taskForm.get('subTasks') as FormArray;
+  }
+
+  get tags(): FormArray {
+    return this.taskForm.get('tags') as FormArray;
+  }
+}
 
